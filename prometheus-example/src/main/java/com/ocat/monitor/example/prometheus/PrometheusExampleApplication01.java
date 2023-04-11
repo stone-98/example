7ffd7c2383e045f5f2c6bfe0281a841cc8bff65a
stone-98 <670569467@qq.com>
2023-04-11 23:16:48 +0800

    feat:更新关于Prometheus基本概念Example

--- prometheus-example/src/main/java/com/ocat/monitor/example/prometheus/PrometheusExampleApplication01.java
+++ prometheus-example/src/main/java/com/ocat/monitor/example/prometheus/PrometheusExampleApplication01.java
@@ -1,54 +1,55 @@
 package com.ocat.monitor.example.prometheus;
 
 import io.prometheus.client.Collector;
-import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Gauge;
-import io.prometheus.client.exporter.HTTPServer;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Hello world!
+ * Gauge与MetricFamilySamples是一对一的关系
  */
 public class PrometheusExampleApplication01 {
+    
     public static void main(String[] args) {
-//        DefaultExports.initialize();
-//        CollectorRegistry registry = new CollectorRegistry();
-        // 创建一个 Gauge 类型的度量
         Gauge requestTotal = Gauge.build().name("request_total") // 指标名称
                 .help("request total") // 指标帮助信息
-                .labelNames("type") // 指标标签
+                .labelNames("type", "status") // 指标标签
                 .register();
-        List<Collector.MetricFamilySamples> collect = requestTotal.collect();
         // 定义度量信息
         long total = 100;
         long success = 99;
         long fail = 1;
-
+        
         // 添加度量数据
-        requestTotal.labels("total").set(total);
-        requestTotal.labels("success").set(success);
-        requestTotal.labels("fail").set(fail);
-
-        // 创建采集器
-        Collector collector = new Collector() {
-            // 采集指标信息
-            public List<MetricFamilySamples> collect() {
-                List<io.prometheus.client.Collector.MetricFamilySamples> mfs = new ArrayList<>();
-                return mfs;
+        requestTotal.labels("GET", "200").set(total);
+        requestTotal.labels("POST", "404").set(success);
+        requestTotal.labels("PUT", "500").set(fail);
+        
+        List<Collector.MetricFamilySamples> metricFamilySamples = requestTotal.collect();
+        for (Collector.MetricFamilySamples metricFamilySample : metricFamilySamples) {
+            // 名称
+            System.out.println("MetricFamilySamples.name:" + metricFamilySample.name);
+            // 类型
+            System.out.println("MetricFamilySamples.type:" + metricFamilySample.type);
+            // 单位
+            System.out.println("MetricFamilySamples.unit:" + metricFamilySample.unit);
+            // 帮助文本
+            System.out.println("MetricFamilySamples.help:" + metricFamilySample.help);
+            for (Collector.MetricFamilySamples.Sample sample : metricFamilySample.samples) {
+                // 此name与MetricFamilySamples.name对应
+                System.out.println("Sample.name:" + sample.name);
+                // labelName
+                System.out.println("Sample.labelNames:" + sample.labelNames);
+                // labelValue
+                System.out.println("Sample.labelValues:" + sample.labelValues);
+                // value：标记的指标值，与 sample 中的值相同。
+                // timestamp：标记的时间戳，用于指示该指标值是何时采集的。
+                // labels：可选标记的标签。示例标签用于标记采样数据的上下文，通常是采样值的来源、处理或相关性。
+                System.out.println("Sample.exemplar:" + sample.exemplar);
+                // 该度量的采集时间
+                System.out.println("Sample.timestampMs:" + sample.timestampMs);
+                
             }
-        };
-
-        // 注册采集器
-        CollectorRegistry.defaultRegistry.register(collector);
-
-        // 创建 HTTP 服务器并暴露度量信息
-        try {
-            new HTTPServer(8080);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
