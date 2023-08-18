05349d2d031d82a0ea3e48e31af378bf9f89c5e4
stone-98 <670569467@qq.com>
2023-08-18 11:20:33 +0800

    feat(LeetCode): 不同的二叉搜索树II_95_回溯算法

---
+++ leetcode/src/main/java/com/ocat/example/leetcode/不同的二叉搜索树II_95/TreeNode.java
@@ -0,0 +1,29 @@
+package com.ocat.example.leetcode.不同的二叉搜索树II_95;
+
+/**
+ * @author stone-98
+ * @date 2023/8/18
+ */
+public class TreeNode {
+    
+    int val;
+    
+    TreeNode left;
+    
+    TreeNode right;
+    
+    TreeNode() {
+    }
+    
+    TreeNode(int val) {
+        this.val = val;
+    }
+    
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+    
+    
+}
